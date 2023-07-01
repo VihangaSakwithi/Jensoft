6ecde675549866e32b60b8b414e7e47deec3cd5a
Vihanga Sakwithi <vsakwithi@outlook.com>
2023-07-01 17:32:07 +0530

    Fronend part i

--- Jeansoft/urls.py
+++ Jeansoft/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
      path('', include('portfolio.urls')),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
